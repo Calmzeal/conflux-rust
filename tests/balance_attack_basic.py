702bd80632530208524aaccc0e90f351eb731697
CalmZeal <enlin3hao@163.com>
2019-09-24 17:34:38 +0800

    Replace basic attack with the multi node version

--- tests/balance_attack_basic.py
+++ tests/balance_attack_basic.py
@@ -1,42 +1,66 @@
 #!/usr/bin/env python3
+
+''' 2019-09-22, 09-23, 09-24
+    Log :
+    CurrentCode = functionality_substitution (annotation (Peilun's Code) )
+
+    Libraries introduction:
+
+    argparse - help rpc parse
+    http - conection
+    eth,conflux - block info storage <> comm message typing changer 
+    test_framework - including tools trigguering each phase in blockchain network establishment
+'''
+from random import randint
 from argparse import ArgumentParser
 from http.client import CannotSendRequest
-
 from eth_utils import decode_hex
-
 from conflux.utils import encode_hex, privtoaddr, parse_as_int
 from test_framework.blocktools import create_transaction, create_block
 from test_framework.test_framework import ConfluxTestFramework
 from test_framework.mininode import *
 from test_framework.util import *
+# Warning: lot of unuseful import
 
+
+''' Def P2PTest [X : <BlockchainType>TestFramework -> Y: Class(_Auxiliary, f1, f2, f3)]
+    A 3 phase attacking test over a given blockchain test framework,
+    including setup_chain, setup_network, and run_test.
+'''
 class P2PTest(ConfluxTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
-        self.num_nodes = 2
+        # Modifiable: v.Peilun 2 -> v.Enlin 4
+        self.num_nodes = 4
         self.start_attack = True
+        
 
     def add_options(self, parser:ArgumentParser):
         parser.add_argument(
             "--evil",
             dest="evil_rate",
+            # Modifiable: the minig power of the adversary part
             default=0.2,
             type=float,
         )
 
     def setup_chain(self):
         self.log.info("Initializing test directory " + self.options.tmpdir)
+        # Modifiable: the expected block generation rate for the whole chain
         self.total_period = 0.25
         self.evil_rate = self.options.evil_rate
-        self.difficulty = int(2 / (1/self.total_period) / (1-self.evil_rate) * 100)
+        # Modifiable: v.Peilun 2 -> v.Enlin num_nodes
+        self.difficulty = int(self.num_nodes / (1/self.total_period) / (1-self.evil_rate) * 100)
+        ''' Exp Proposition : The higher the difficulty is, the less probable to fork
+        '''
+        #self.difficulty = 75    
         print(self.difficulty)
         self.conf_parameters = {
             "start_mining": "true",
             "initial_difficulty": str(self.difficulty),
-            "test_mining_sleep_us": "10000",
             "mining_author": '"' + "0"*40 + '"',
             "log_level": "\"debug\"",
-            "headers_request_timeout_ms": "30000",  # need to be larger than network latency
+            "headers_request_timeout_ms": "40000",  # need to be larger than network latency [What is it?]
             "heavy_block_difficulty_ratio": "1000",  # parameter used in the original experiments
             "adaptive_weight_beta": "3000",  # parameter used in the original experiments
         }
@@ -44,26 +68,32 @@ class P2PTest(ConfluxTestFramework):
 
     def setup_network(self):
         self.setup_nodes()
-        connect_nodes(self.nodes, 0, 1)
-
-        # Set latency between two groups
-        self.nodes[0].addlatency(self.nodes[1].key, 10000)
-        self.nodes[1].addlatency(self.nodes[0].key, 10000)
+        ''' Set the topology of the network: complete graph;
+            Then set the latency over the topology
+        '''
+        for i in range(self.num_nodes-1):
+            for j in range(i+1,self.num_nodes):
+                connect_nodes(self.nodes, i, j)
+        for i in range(self.num_nodes):
+            for j in range(self.num_nodes):
+                if i != j:
+                    self.nodes[i].addlatency(self.nodes[j].key, 10000)
 
     def run_test(self):
         start_p2p_connection(self.nodes)
-
-        # Some blocks may have been mined before we setup the latency,
-        # so wait for the latency and find the fork point
         generation_period = 1/(1/self.total_period * self.evil_rate)
         self.log.info("Adversary mining average period=%f", generation_period)
-
-        # Some blocks may have been mined before we setup the latency,
-        # so wait for the latency and find the fork point
+        
+        # Some blocks may have been mined before we setup the latency, [which code corresponds to the decalaration?]
+        # so wait for the latency and find the fork point, [which code corresponds to the decalaration?]
+        ''' Phase 1: Scan the chain every 0.1s until find a fork at a valid height
+            between nodes[0] and nodes[branch_leader]
+        '''
         finished = False
+        branch_leader = int(self.num_nodes/2)
         while not finished:
-            chain0 = self.process_chain(self.nodes[0].getPivotChainAndWeight())
-            chain1 = self.process_chain(self.nodes[1].getPivotChainAndWeight())
+            chain0 = self._process_chain(self.nodes[0].getPivotChainAndWeight())
+            chain1 = self._process_chain(self.nodes[branch_leader].getPivotChainAndWeight())
             fork_height = 0
             while True:
                 if chain0[fork_height][0] != chain1[fork_height][0]:
@@ -83,17 +113,22 @@ class P2PTest(ConfluxTestFramework):
         else:
             receipts_root = default_config["GENESIS_RECEIPTS_ROOT"]
 
+        ''' Phase 2: At each given adversary block generation moment (decided by adversary's mining power),
+            send blocks to each fork until they merge and are stablized. 
+        '''
         count = 0
         after_count = 0
         merged = False
         while True:
-            # This roughly determines adversary's mining power
+            sample0 = randint(0,branch_leader-1)
+            sample1 = randint(branch_leader,self.num_nodes-1)
+            print(sample0, sample1)
+            # This roughly simulates adversary's mining power
             time.sleep(random.expovariate(1 / generation_period))
-
-            chain0 = self.process_chain(self.nodes[0].getPivotChainAndWeight())
-            self.check_chain_heavy(chain0, 0, fork_height)
-            chain1 = self.process_chain(self.nodes[1].getPivotChainAndWeight())
-            self.check_chain_heavy(chain1, 1, fork_height)
+            chain0 = self._process_chain(self.nodes[sample0].getPivotChainAndWeight())
+            self._check_chain_heavy(chain0, 0, fork_height)
+            chain1 = self._process_chain(self.nodes[sample1].getPivotChainAndWeight())
+            self._check_chain_heavy(chain1, 1, fork_height)
             assert_equal(chain0[0][0], chain1[0][0])
             fork0 = chain0[fork_height]
             fork1 = chain1[fork_height]
@@ -124,24 +159,25 @@ class P2PTest(ConfluxTestFramework):
                     else:
                         send1 = False
                 if send1:
+                    
                     parent = fork0[0]
                     block = NewBlock(create_block(decode_hex(parent), height=fork_height+1, deferred_receipts_root=receipts_root, difficulty=self.difficulty, timestamp=int(time.time()), author=decode_hex("%040x" % random.randint(0, 2**32 - 1))))
-                    self.nodes[0].p2p.send_protocol_msg(block)
+                    self.nodes[sample0].p2p.send_protocol_msg(block)
                     self.log.debug("send to 0 block %s, weight %d %d", block.block.hash_hex(), fork0[1], fork1[1])
                 else:
                     parent = fork1[0]
                     block = NewBlock(create_block(decode_hex(parent), height=fork_height+1, deferred_receipts_root=receipts_root, difficulty=self.difficulty, timestamp=int(time.time()), author=decode_hex("%040x" % random.randint(0, 2**32 - 1))))
-                    self.nodes[1].p2p.send_protocol_msg(block)
+                    self.nodes[sample1].p2p.send_protocol_msg(block)
                     self.log.debug("send to 1 block %s, weight %d %d", block.block.hash_hex(), fork0[1], fork1[1])
         exit()
 
     # Convert weight to integer
-    def process_chain(self, chain):
+    def _process_chain(self, chain):
         for i in range(len(chain)):
             chain[i][1] = parse_as_int(chain[i][1])
         return chain
 
-    def check_chain_heavy(self, chain, chain_id, fork_height):
+    def _check_chain_heavy(self, chain, chain_id, fork_height):
         for i in range(fork_height+1, len(chain)-1):
             if chain[i][1] - chain[i+1][1] >= self.difficulty * 240:
                 self.log.debug("chain %d is heavy at height %d %d %d", chain_id, i,  chain[i][1], chain[i+1][1])
